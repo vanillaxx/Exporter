a990663984d0d53b6483cf2711146d3b903c4595
frozanna <ania.s.731@gmail.com>
2020-12-09 22:20:46 +0100

    Small fix for windows script

--- scripts/start.bat
+++ scripts/start.bat
@@ -2,7 +2,7 @@ SET _path=%cd%
 for %%a in ("%_path%") do set "ROOT_DIR=%%~dpa"
 
 IF not exist %ROOT_DIR%env (
-    install.bat
+    CALL install.bat
 )
 
 cd ../ & %ROOT_DIR%env\Scripts\python %ROOT_DIR%manage.py runserver
